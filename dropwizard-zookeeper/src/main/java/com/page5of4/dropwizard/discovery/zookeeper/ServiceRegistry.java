19b90a269b4ce17048d71a718379337961292203
Jacob Lewallen <jlewallen@gmail.com>
2014-05-17 14:58:34 -0700

    Can get lists of service instances.

--- dropwizard-zookeeper/src/main/java/com/page5of4/dropwizard/discovery/zookeeper/ServiceRegistry.java
+++ dropwizard-zookeeper/src/main/java/com/page5of4/dropwizard/discovery/zookeeper/ServiceRegistry.java
@@ -1,15 +1,18 @@
 package com.page5of4.dropwizard.discovery.zookeeper;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import io.dropwizard.lifecycle.Managed;
 import org.apache.curator.framework.CuratorFramework;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 public class ServiceRegistry implements Managed {
    private static ServiceRegistry singleton;
    private final CuratorFramework curatorClient;
-   private final List<Managed> services = Lists.newArrayList();
+   private final Map<Class<?>, ServicePublisher<?>> services = Maps.newHashMap();
    private final String path;
    private boolean started;
 
@@ -24,9 +27,23 @@ public class ServiceRegistry implements Managed {
    }
 
    @SuppressWarnings("unchecked")
+   public <T> Collection<T> getServices(Class<T> klass) {
+      List<T> instances = Lists.newArrayList();
+      for(Object service : services.get(klass).getServices()) {
+         instances.add(klass.cast(service));
+      }
+      return instances;
+   }
+
+   @SuppressWarnings("unchecked")
+   public <T> void publish(T service) {
+      publish(service.getClass().getName(), service);
+   }
+
+   @SuppressWarnings("unchecked")
    public <T> void publish(String name, T service) {
       ServicePublisher<T> publisher = new ServicePublisher<T>(curatorClient, path, name, (Class<T>)service.getClass(), service);
-      services.add(publisher);
+      services.put(service.getClass(), publisher);
       if(started) {
          try {
             publisher.start();
@@ -39,7 +56,7 @@ public class ServiceRegistry implements Managed {
 
    @Override
    public void start() throws Exception {
-      for(Managed service : services) {
+      for(Managed service : services.values()) {
          service.start();
       }
       started = true;
@@ -47,7 +64,7 @@ public class ServiceRegistry implements Managed {
 
    @Override
    public void stop() throws Exception {
-      for(Managed service : services) {
+      for(Managed service : services.values()) {
          service.stop();
       }
    }
