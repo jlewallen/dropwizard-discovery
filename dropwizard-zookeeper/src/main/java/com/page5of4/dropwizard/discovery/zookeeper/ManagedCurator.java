bd4bffa50a0f92a677d152846c9387ca3710a3b5
Jacob Lewallen <jlewallen@gmail.com>
2014-05-18 22:19:44 -0700

    ServiceRegistry listing works now w/o having to publish.

--- dropwizard-zookeeper/src/main/java/com/page5of4/dropwizard/discovery/zookeeper/ManagedCurator.java
+++ dropwizard-zookeeper/src/main/java/com/page5of4/dropwizard/discovery/zookeeper/ManagedCurator.java
@@ -5,8 +5,11 @@ import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.curator.utils.CloseableUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ManagedCurator implements Managed {
+   private static final Logger logger = LoggerFactory.getLogger(ManagedCurator.class);
    private final CuratorFramework curatorClient;
 
    public CuratorFramework getCuratorClient() {
@@ -19,6 +22,7 @@ public class ManagedCurator implements Managed {
 
    @Override
    public void start() throws Exception {
+      logger.info("Starting Curator");
       curatorClient.start();
    }
 
--- dropwizard-zookeeper/src/main/java/com/page5of4/dropwizard/discovery/zookeeper/ServiceRegistry.java
+++ dropwizard-zookeeper/src/main/java/com/page5of4/dropwizard/discovery/zookeeper/ServiceRegistry.java
@@ -4,7 +4,13 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import io.dropwizard.lifecycle.Managed;
 import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.utils.CloseableUtils;
+import org.apache.curator.x.discovery.ServiceDiscovery;
+import org.apache.curator.x.discovery.ServiceDiscoveryBuilder;
+import org.apache.curator.x.discovery.ServiceInstance;
+import org.apache.curator.x.discovery.details.JsonInstanceSerializer;
 
+import java.io.Closeable;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -12,7 +18,7 @@ import java.util.Map;
 public class ServiceRegistry implements Managed {
    private static ServiceRegistry singleton;
    private final CuratorFramework curatorClient;
-   private final Map<Class<?>, ServicePublisher<?>> services = Maps.newHashMap();
+   private final Map<Class<?>, ServiceDiscovery<?>> discoveryCache = Maps.newHashMap();
    private final String path;
    private boolean started;
 
@@ -29,10 +35,44 @@ public class ServiceRegistry implements Managed {
    @SuppressWarnings("unchecked")
    public <T> Collection<T> getServices(Class<T> klass) {
       List<T> instances = Lists.newArrayList();
-      for(Object service : services.get(klass).getServices()) {
+      for(Object service : getServices(klass, klass.getName())) {
          instances.add(klass.cast(service));
       }
       return instances;
+   }
+
+   @SuppressWarnings("unchecked")
+   private <T> ServiceDiscovery<T> getServiceDiscovery(Class<T> klass, String name, T self) {
+      if(discoveryCache.containsKey(klass)) {
+         return (ServiceDiscovery<T>)discoveryCache.get(klass);
+      }
+      try {
+         JsonInstanceSerializer<T> serializer = new JsonInstanceSerializer<T>(klass);
+         ServiceInstance<T> selfInstance = self == null ? null : ServiceInstance.<T>builder().name(name).payload(self).port(0).build();
+         ServiceDiscovery<T> serviceDiscovery = ServiceDiscoveryBuilder.builder(klass).client(curatorClient).basePath(path).serializer(serializer).thisInstance(selfInstance).build();
+         discoveryCache.put(klass, serviceDiscovery);
+         if(started) {
+            serviceDiscovery.start();
+         }
+         return serviceDiscovery;
+      }
+      catch(Exception e) {
+         throw new RuntimeException(e);
+      }
+   }
+
+   private <T> Collection<T> getServices(Class<T> klass, String name) {
+      try {
+         Collection<T> services = Lists.newArrayList();
+         Collection<ServiceInstance<T>> instances = getServiceDiscovery(klass, name, null).queryForInstances(name);
+         for(ServiceInstance<T> instance : instances) {
+            services.add(instance.getPayload());
+         }
+         return services;
+      }
+      catch(Exception e) {
+         throw new RuntimeException(e);
+      }
    }
 
    @SuppressWarnings("unchecked")
@@ -42,30 +82,21 @@ public class ServiceRegistry implements Managed {
 
    @SuppressWarnings("unchecked")
    public <T> void publish(String name, T service) {
-      ServicePublisher<T> publisher = new ServicePublisher<T>(curatorClient, path, name, (Class<T>)service.getClass(), service);
-      services.put(service.getClass(), publisher);
-      if(started) {
-         try {
-            publisher.start();
-         }
-         catch(Exception e) {
-            throw new RuntimeException(e);
-         }
-      }
+      getServiceDiscovery((Class<T>)service.getClass(), name, service);
    }
 
    @Override
    public void start() throws Exception {
-      for(Managed service : services.values()) {
-         service.start();
+      for(ServiceDiscovery<?> serviceDiscovery : discoveryCache.values()) {
+         serviceDiscovery.start();
       }
       started = true;
    }
 
    @Override
    public void stop() throws Exception {
-      for(Managed service : services.values()) {
-         service.stop();
+      for(Closeable serviceDiscovery : discoveryCache.values()) {
+         CloseableUtils.closeQuietly(serviceDiscovery);
       }
    }
 }
